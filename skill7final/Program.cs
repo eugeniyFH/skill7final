a6726d7077a918b8589493f3b820ff1040079675
eugeniyFH <shadrin_evgeni07@mail.ru>
2025-01-14 02:51:49 +0700

    Добавьте файлы проекта.

---
+++ skill7final/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace skill7final
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    abstract class Delivery
+    {
+        public string Address;
+        public abstract void Delivery1();
+        Random random = new Random();
+    }
+
+    class HomeDelivery : Delivery
+    {
+        public override void Delivery1()
+        {
+            string Address = Console.ReadLine();
+            Console.WriteLine(Address);
+        }
+    }
+
+    class PickPointDelivery : Delivery
+    {
+        public override void Delivery1()
+        {
+            Random random = new Random();
+            string[] addresspoint = new string[random.Next(50)];
+            foreach (string s in addresspoint) { Console.WriteLine(s); }
+            Console.WriteLine($"Выберите аресс доставки в виде числа от 1 до {addresspoint.Length}");
+            int count = int.Parse(Console.ReadLine());
+            Console.WriteLine(addresspoint[count]);
+        }
+    }
+
+    class ShopDelivery : Delivery
+    {
+        public override void Delivery1()
+        {
+            Random random = new Random();
+            string[] addressshop = new string[random.Next(15)];
+            foreach (string s in addressshop){ Console.WriteLine(s); }
+            Console.WriteLine($"Выберите аресс доставки в виде числа от 1 до {addressshop.Length}");
+            int count = int.Parse(Console.ReadLine());
+            Console.WriteLine(addressshop[count]);
+        }
+    }
+
+    class Order<TDelivery,
+    TStruct> where TDelivery : Delivery
+    {
+        public TDelivery Delivery;
+
+        public int Number;
+
+        public string Description;
+
+        public void DisplayAddress()
+        {
+            Console.WriteLine(Delivery.Address);
+        }
+        public Product product {  get; set; }
+        // ... Другие поля
+    }
+    abstract class Product
+    {
+        public string product { get; set; }
+        public abstract void Package();
+    }
+    class FragleProduct<T>:Product
+    {
+        public override void Package()
+        {
+            Console.WriteLine("Упаковать дополнительную защиту ");
+        }
+    }
+    class PerishableProduct<T> : Product 
+    {
+        public override void Package()
+        {
+            Console.WriteLine("Поместить в холодильник");
+        }
+    }
+}
---
+++ skill7final/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("skill7final")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("skill7final")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("42f0e3d6-5dd8-4b86-888d-7bf35e0de385")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
